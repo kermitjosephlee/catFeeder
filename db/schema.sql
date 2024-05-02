6cfb2e68ff7d73c87df310b4532ca67e6afcfa05
Kermit Joseph Lee <37561135+kermitjosephlee@users.noreply.github.com>
2024-05-02 11:06:12 -0400

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,6 @@
 DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS ingredients;
+DROP TABLE IF EXISTS product_ingredients;
 
 CREATE TABLE IF NOT EXISTS products (
   id SERIAL PRIMARY KEY,
@@ -10,3 +12,37 @@ CREATE TABLE IF NOT EXISTS products (
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS ingredients (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS product_ingredients (
+  id SERIAL PRIMARY KEY,
+  product_id INT NOT NULL,
+  ingredient_id INT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP
+);
+
+-- INSERT INTO ingredients (name)
+-- SELECT DISTINCT LOWER(TRIM(ingredient))
+-- FROM (
+--     SELECT unnest(string_to_array(ingredients, ',')) AS ingredient
+--     FROM products
+-- ) AS subquery
+-- WHERE TRIM(ingredient) <> '';
+
+-- INSERT INTO product_ingredients (product_id, ingredient_id)
+-- SELECT 
+--     products.id AS product_id,
+--     ingredients.id AS ingredient_id
+-- FROM 
+--     products
+-- CROSS JOIN LATERAL 
+--     unnest(string_to_array(products.ingredients, ',')) AS ingredient_name
+-- JOIN 
+--     ingredients ON TRIM(ingredients.name) = TRIM(ingredient_name);
---
+++ seedDB.js
@@ -0,0 +1,124 @@
+import "dotenv/config";
+import querystring from "node:querystring";
+import axios from "axios";
+import * as cheerio from "cheerio";
+import delay from "delay";
+import pg from "pg";
+
+export const pgCredentials = process.env.DB_CREDENTIALS;
+
+const pool = new pg.Pool({
+  connectionString: pgCredentials,
+});
+await pool.connect();
+
+const REQUEST_DELAY = 50;
+
+const skipProducts = [
+  "product/fancy feast/Medleys+White+Meat+Chicken+Florentine+Pate%CC%81+With+Cheese+%26+Spinach",
+  "product/firstmate/Cage+Free+Chicken+%26+Wild+Tuna+50%2F50+Formula",
+];
+
+const config = {
+  headers: {
+    "User-Agent":
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
+  },
+};
+
+async function executeProductPromises(hrefs) {
+  let results = [];
+  for (const href of hrefs) {
+    await delay(REQUEST_DELAY);
+    const response = await axios.get(href);
+    const $ = cheerio.load(response.data);
+    const $productHref = $(".col-sm-12 > a")
+      .map((i, el) => {
+        return $(el).attr("href");
+      })
+      .get();
+
+    const filteredResults = $productHref.filter((each) => {
+      return !skipProducts.includes(each);
+    });
+    results = [...results, filteredResults];
+  }
+  return results;
+}
+
+async function executeProductDetailPages(hrefs) {
+  for (const href of hrefs) {
+    const pathname = new URL(href).pathname;
+
+    const brand = querystring
+      .unescape(pathname.split("/")[2])
+      .replaceAll("+", " ");
+    const productName = querystring
+      .unescape(pathname.split("/")[3])
+      .replaceAll("+", " ");
+
+    await delay(REQUEST_DELAY);
+    try {
+      const response = await axios.get(href);
+
+      const $ = cheerio.load(response.data);
+      const productImage = $(".col-sm-4 .thumbnail").attr("src");
+      const chewyLink = $(".col-sm-4 > a.amazon-product").attr("href")
+        ? $(".col-sm-4 > a.amazon-product").attr("href").replace("pn?url=", "")
+        : "";
+
+      const ingredientsTitle = $('h3:contains("Ingredients")');
+      const ingredientsList = ingredientsTitle.next().next().text();
+
+      const queryText = `INSERT INTO products(brand, name, image_url, link_url, ingredients) VALUES ($1, $2, $3, $4, $5) RETURNING *`;
+      const queryValues = [
+        brand,
+        productName,
+        productImage,
+        chewyLink,
+        ingredientsList,
+      ];
+
+      const res = await pool.query(queryText, queryValues);
+      console.log(res.rows[0]);
+    } catch (error) {
+      if (error.response && error.response.status === 404){
+        continue;
+      } else {
+        console.log("ERRORZ:", error);
+      }
+    }
+  }
+}
+
+axios
+  .get("https://catfooddb.com", config)
+  .then((response) => {
+    const $ = cheerio.load(response.data);
+    const $catFood = $(
+      ".navbar > .container-fluid > .navbar-collapse > .navbar-nav > li > .dropdown-menu > .dropdown-submenu > .dropdown-menu > li > a",
+    );
+    const $catFoodHrefs = $catFood
+      .map((i, el) => {
+        return $(el).attr("href");
+      })
+      .get();
+
+    const catFoodHrefs = $catFoodHrefs.map((each) => {
+      return `https://catfooddb.com/${each}`;
+    });
+
+    return catFoodHrefs;
+  })
+  .then(async (catFoodHrefs) => {
+    const allResults = await executeProductPromises(catFoodHrefs);
+    return allResults;
+  })
+  .then(async (allResults) => {
+    const updatedURLs = allResults.flat().map((each) => {
+      return `https://catfooddb.com/${each}`;
+    });
+    await executeProductDetailPages(updatedURLs);
+  })
+  .then(() => pool.end())
+  .catch((error) => console.log("ERR:", error));
