bfb935fc822d6f4a6971f274113e2dd1a6fef934
Kermit Joseph Lee <37561135+kermitjosephlee@users.noreply.github.com>
2024-05-02 11:46:46 -0400

    added testDB and cors package

--- testDB.js
+++ testDB.js
@@ -3,11 +3,13 @@ import pg from "pg";
 
 export const pgCredentials = process.env.DB_CREDENTIALS;
 
+console.log(pgCredentials);
+
 const pool = new pg.Pool({
   connectionString: pgCredentials,
 });
 await pool.connect();
 
-const res = await pool.query(`SELECT * FROM products LIMIT 10;`);
-console.log(res);
+const res = await pool.query(`SELECT * FROM ingredients LIMIT 10;`);
+console.log(res.rows);
 pool.end();
