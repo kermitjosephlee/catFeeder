d35ab642de220e5f68e97d9c0c0a67c9a58381ec
Kermit Joseph Lee <37561135+kermitjosephlee@users.noreply.github.com>
2024-05-14 12:00:03 -0400

    working on docker

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,6 +5,16 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      node: {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/node/, ""),
+        secure: false,
+      },
+    },
+  },
   resolve: {
     alias: {
       "@components": path.resolve(__dirname, "src/components"),
