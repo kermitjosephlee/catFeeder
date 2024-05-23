9d40942fcb808007e2c15629ddb0047d47d7992d
Kermit Joseph Lee <37561135+kermitjosephlee@users.noreply.github.com>
2024-05-23 17:47:50 -0400

    updated gitignore to remove logs

--- api/db/queryHelpers/ingredients.js
+++ api/db/queryHelpers/ingredients.js
@@ -1,24 +1,24 @@
 import format from "pg-format";
 
 const queryArrayBuilder = (arr) => {
-  return arr.map((each) => `'${each}'`);
+	return arr.map((each) => `'${each}'`);
 };
 
 export const ingredientsJoinQueryBuilder = ({
-  includeIngredients,
-  excludeIngredients,
+	includeIngredients,
+	excludeIngredients,
 }) => {
-  const formattedIncludeIngredients =
-    queryArrayBuilder(includeIngredients) || [];
-  const formattedExcludeIngredients =
-    queryArrayBuilder(excludeIngredients) || [];
+	const formattedIncludeIngredients =
+		queryArrayBuilder(includeIngredients) || [];
+	const formattedExcludeIngredients =
+		queryArrayBuilder(excludeIngredients) || [];
 
-  const hasIncludeIngredients = formattedIncludeIngredients.length > 0;
-  const hasExcludeIngredients = formattedExcludeIngredients.length > 0;
-  const hasBothIncludeAndExcludeIngredients =
-    hasIncludeIngredients && hasExcludeIngredients;
+	const hasIncludeIngredients = formattedIncludeIngredients.length > 0;
+	const hasExcludeIngredients = formattedExcludeIngredients.length > 0;
+	const hasBothIncludeAndExcludeIngredients =
+		hasIncludeIngredients && hasExcludeIngredients;
 
-  const includeSubQuery = `NOT EXISTS (
+	const includeSubQuery = `NOT EXISTS (
       SELECT 1 FROM UNNEST(ARRAY[${formattedIncludeIngredients}]) AS required_ingredient
       WHERE NOT EXISTS (
           SELECT 1
@@ -29,7 +29,7 @@ export const ingredientsJoinQueryBuilder = ({
       )
   )`;
 
-  const excludeSubQuery = `NOT EXISTS (
+	const excludeSubQuery = `NOT EXISTS (
       SELECT 1 FROM UNNEST(ARRAY[${formattedExcludeIngredients}]) AS unwanted_ingredient
       WHERE EXISTS (
           SELECT 1
@@ -40,7 +40,7 @@ export const ingredientsJoinQueryBuilder = ({
       )
   )`;
 
-  const formattedQuery = format(`SELECT products.*
+	const formattedQuery = format(`SELECT products.*
   FROM products
   WHERE
   ${hasIncludeIngredients ? includeSubQuery : ""}
@@ -48,43 +48,48 @@ export const ingredientsJoinQueryBuilder = ({
   ${hasExcludeIngredients ? excludeSubQuery : ""}
  ;`);
 
-  return formattedQuery;
+	return formattedQuery;
 };
 
 export const ingredientsQueryBuilder = ({
-  includeIngredients,
-  excludeIngredients,
+	includeIngredients,
+	excludeIngredients,
 }) => {
-  const formattedIncludeIngredients =
-    queryArrayBuilder(includeIngredients) || [];
-  const formattedExcludeIngredients =
-    queryArrayBuilder(excludeIngredients) || [];
+	const formattedIncludeIngredients =
+		queryArrayBuilder(includeIngredients) || [];
+	const formattedExcludeIngredients =
+		queryArrayBuilder(excludeIngredients) || [];
 
-  const hasIncludeIngredients = formattedIncludeIngredients.length > 0;
-  const hasExcludeIngredients = formattedExcludeIngredients.length > 0;
-  const hasBothIncludeAndExcludeIngredients =
-    hasIncludeIngredients && hasExcludeIngredients;
+	const hasIncludeIngredients = formattedIncludeIngredients.length > 0;
+	const hasExcludeIngredients = formattedExcludeIngredients.length > 0;
+	const hasBothIncludeAndExcludeIngredients =
+		hasIncludeIngredients && hasExcludeIngredients;
 
-  const includeSubQuery = hasIncludeIngredients
-    ? `(${formattedIncludeIngredients.map(
-        (ingredient) => `LOWER(products.ingredients) LIKE '%' || LOWER(${ingredient}) || '%'`
-      ).join(" OR ")})`
-    : "";
-  
-  const excludeSubQuery = hasExcludeIngredients
-    ? `NOT (${formattedExcludeIngredients.map(
-        (ingredient) => `LOWER(products.ingredients) LIKE '%' || LOWER(${ingredient}) || '%'`
-      ).join(" OR ")})`
-    : "";
+	const includeSubQuery = hasIncludeIngredients
+		? `(${formattedIncludeIngredients
+				.map(
+					(ingredient) =>
+						`LOWER(products.ingredients) LIKE '%' || LOWER(${ingredient}) || '%'`
+				)
+				.join(" OR ")})`
+		: "";
 
-    const formattedQuery = `SELECT products.*
+	const excludeSubQuery = hasExcludeIngredients
+		? `NOT (${formattedExcludeIngredients
+				.map(
+					(ingredient) =>
+						`LOWER(products.ingredients) LIKE '%' || LOWER(${ingredient}) || '%'`
+				)
+				.join(" OR ")})`
+		: "";
+
+	const formattedQuery = `SELECT products.*
     FROM products
     ${hasIncludeIngredients || hasExcludeIngredients ? " WHERE " : ""}
     ${includeSubQuery}
     ${hasBothIncludeAndExcludeIngredients ? " AND " : ""}
     ${excludeSubQuery}
-    LIMIT 50
     ;`;
-    
-    return formattedQuery;
+
+	return formattedQuery;
 };
